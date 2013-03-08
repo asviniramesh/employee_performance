1af64203eab16a24ba6511a01511c597c607404b
asvini.ramesh <asvini.ramesh@revenuemed.com>
2013-03-08 09:18:07 +0530

    layouts added

--- app/models/employee_heirarchy.rb
+++ app/models/employee_heirarchy.rb
@@ -1,4 +1,5 @@
 class EmployeeHeirarchy < ActiveRecord::Base
+attr_accessible :employee_id, :manager_id
 
 scope :employees,  lambda {|employee_id| {:conditions => ['employee_id = ?',employee_id] }}
 scope :managers, lambda {|manager_id| {:conditions => ['manager_id = ?',manager_id] }}
