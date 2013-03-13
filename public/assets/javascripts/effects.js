cd96ba0288f78d339d0454f2e6967b296a798d78
asvini.ramesh <asvini.ramesh@revenuemed.com>
2013-03-13 15:51:35 +0530

    Commit Three

--- public/assets/javascripts/effects.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-function showFrontLayer() {
-				document.getElementById('bg_mask').style.visibility='visible';
-				document.getElementById('frontlayer').style.visibility='visible';
-		    }
-			function hideFrontLayer() {
-				document.getElementById('bg_mask').style.visibility='hidden';
-				document.getElementById('frontlayer').style.visibility='hidden';
-		    }
