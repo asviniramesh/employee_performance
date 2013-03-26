7d9e451d1c5be9e3dc1191a007db2c8fac7ef49b
asvini.ramesh <asvini.ramesh@revenuemed.com>
2013-03-26 11:15:13 +0530

    float type changed

--- db/migrate/20130320070224_change_data_type_for_boolean.rb
+++ db/migrate/20130320070224_change_data_type_for_boolean.rb
@@ -1,6 +1,6 @@
 class ChangeDataTypeForBoolean < ActiveRecord::Migration
   def up
-  change_column :evaluation_frequencies, :format, :decimal
+  change_column :evaluation_frequencies, :format, :string
   end
 
   def down
