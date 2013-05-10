fb518c731db2995815ecf8af8648370cd03542d8
asvini.ramesh <asvini.ramesh@revenuemed.com>
2013-05-10 11:35:39 +0530

    populating values

--- app/models/value.rb
+++ app/models/value.rb
@@ -4,6 +4,6 @@ class Value < ActiveRecord::Base
   has_many :evaluation_intervals
 
   def get_current_evaluations employee
-   self.evaluations.where('employee_id = ? and created_at >= ? and created_at < ?', employee.id, Time.parse('2013-04-26'), Time.parse('2013-05-10')).first
+   self.evaluations.where('employee_id = ? and created_at >= ? and created_at < ?', employee.id, Time.parse('2013-05-10'), Time.parse('2013-05-24')).first
   end
 end
