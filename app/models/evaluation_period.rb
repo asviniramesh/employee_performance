f644eb10bc04bf5cbc24d86d8dc725ef8590279a
asvini.ramesh <asvini.ramesh@revenuemed.com>
2013-06-05 17:30:24 +0530

    eval period

--- app/models/evaluation_period.rb
+++ app/models/evaluation_period.rb
@@ -1,5 +1,46 @@
 class EvaluationPeriod < ActiveRecord::Base
-	attr_accessible :evaluations_ids, :Final_eval_end_date
+	attr_accessible :evaluations_ids, :Final_eval_end_date, :final_eval_start_date, :manager_eval_end_date, :manager_eval_start_date, :self_eval_end_date, :self_eval_start_date, :evaluation_frequency_id
 	  has_many :evaluations
 		belongs_to :evaluation_intervals
+		belongs_to :evaluation_frequency
+
+  rails_admin do
+    create do
+      field :self_eval_start_date do
+				label "Start Date"
+			end
+			field :self_eval_end_date do
+				label "End Date"
+			end
+			field :evaluation_frequency_id do
+				label "Evaluation Frequency"
+			end
+    end
+		list do
+			field :id do
+				label "Id"
+			end
+		 	field :self_eval_start_date do
+				label "Start Date"
+			end
+			field :self_eval_end_date do
+				label "End Date"
+			end
+			field :evaluation_frequency_id do
+				label "Evaluation Frequency"
+			end
+		end
+  
+	 	edit do
+      field :self_eval_start_date do
+				label "Start Date"
+			end
+			field :self_eval_end_date do
+				label "End Date"
+			end
+			field :evaluation_frequency_id do
+				label "Evaluation Frequency"
+			end
+    end
+end
 end
